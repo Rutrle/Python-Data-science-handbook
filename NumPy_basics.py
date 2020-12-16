81313a617b894f94aadc56f8dd1757fa8895cb7a
Jakub <rutrle@seznam.cz>
2020-12-16 23:38:29 +0100

    creating NumPy array

--- NumPy_basics.py
+++ NumPy_basics.py
@@ -3,7 +3,23 @@ import array
 
 print(np.__version__)
 
+# python buil-in array
 
 L = list(range(10))
 A = array.array('i', L)
 print(A)
+
+
+# Numpy arrays
+
+# integer array
+B = np.array([1, 4, 2, 5, 3])
+print(B)
+
+# upcast from integer to float
+C = np.array([3.14, 5, 6, 4])
+print(C)
+
+# set type of numpy array:
+D = np.array([1, 2, 3, 4], dtype='float32')
+print(D)
