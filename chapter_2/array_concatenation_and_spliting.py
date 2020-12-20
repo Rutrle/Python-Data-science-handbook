9b02b3fddc12217e33a9e923694e58123f9ef646
Jakub <rutrle@seznam.cz>
2020-12-20 02:27:20 +0100

    splitting arrays

--- chapter_2/array_concatenation_and_spliting.py
+++ chapter_2/array_concatenation_and_spliting.py
@@ -23,6 +23,20 @@ grid = np.array([[9, 8, 7], [6, 5, 4]])
 print(np.vstack([x, grid]))
 
 y = np.array([[99], [99]])
-print(np.hstack([grid, y]))
+print(np.hstack([grid, y]), '\n'*4)
 
+# splitting
+x = [1, 2, 3, 99, 99, 3, 2, 1]
+x1, x2, x3 = np.split(x, [3, 5])  # indices are giving the split points
+print(x1, x2, x3, '\n'*4)
 
+grid = np.arange(16).reshape((4, 4))
+print(grid)
+
+upper, lower = np.vsplit(grid, [2])
+print(upper)
+print(lower)
+
+left, right = np.hsplit(grid, [2])
+print(left)
+print(right)
